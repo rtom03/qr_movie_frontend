635cb686f7fb2f57fe000ecced3de4d72cab2bb8
rtom03 <zuckm709@gmail.com>
2025-06-13 16:04:18 +0100

    implement fetching of random movies

--- src/api.ts
+++ src/api.ts
@@ -1,7 +1,6 @@
 import axios from "axios";
 
-const API_BASE_URL =
-  process.env.REACT_APP_API_BASE_URL || "http://localhost:3000"; // your NestJS backend URL
+const API_BASE_URL = "https://qr-movie-backend.onrender.com"; // your NestJS backend URL
 
 export const fetchQrCode = () => axios.get(`${API_BASE_URL}/movies/qr`);
 export const fetchMovies = (id: string) =>
--- src/components/MovieList.tsx
+++ src/components/MovieList.tsx
@@ -4,13 +4,32 @@ import { fetchMovies } from "../api";
 
 const MovieList: React.FC = () => {
   const { id } = useParams<{ id: string }>();
-  const [movies, setMovies] = useState<string[]>([]);
+  interface Movie {
+    Title: string;
+    Year: string;
+    Images: string[];
+    Released?: string;
+    Genre?: string;
+    Director?: string;
+  }
 
+  const [movies, setMovies] = useState<Movie[]>([]);
+
+  // useEffect(() => {
+  //   const loadMovies = async () => {
+  //     const res = await fetchMovies(id!);
+  //     if (res.data.movies) {
+  //       console.log(movies);
+  //       setMovies(res.data.movies);
+  //     }
+  //   };
+  //   loadMovies();
+  // }, [id]);
   useEffect(() => {
     const loadMovies = async () => {
       const res = await fetchMovies(id!);
       if (res.data.movies) {
-        setMovies(res.data.movies);
+        setMovies(res.data.movies as Movie[]);
       }
     };
     loadMovies();
@@ -21,7 +40,20 @@ const MovieList: React.FC = () => {
       <h1>Here are your movies</h1>
       <ul>
         {movies.map((movie, idx) => (
-          <li key={idx}>{movie}</li>
+          <div key={idx}>
+            <h3>{movie.Title}</h3>
+            <p>Year: {movie.Year}</p>
+            <p>Director: {movie.Director}</p>
+            <p>Genre: {movie.Genre}</p>
+
+            {movie.Images && movie.Images.length > 0 && (
+              <div>
+                {movie.Images.map((img, imgIdx) => (
+                  <img key={imgIdx} src={img} alt={movie.Title} width="150" />
+                ))}
+              </div>
+            )}
+          </div>
         ))}
       </ul>
     </div>
