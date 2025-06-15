64fdd0623814cb62731e067364ccc10ae2e5aaf5
rtom03 <zuckm709@gmail.com>
2025-06-15 07:35:43 +0100

    implement dynamic movies rendering

--- src/api.ts
+++ src/api.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const deployUrl = "https://qr-movie-backend.onrender.com";
-const API_BASE_URL = "http://localhost:8000"; // your NestJS backend URL
+const API_BASE_URL = deployUrl || "http://localhost:8000"; // your NestJS backend URL
 
 export const fetchQrCode = () => axios.get(`${API_BASE_URL}/movies/qr`);
 export const fetchMovies = (id: string) =>
